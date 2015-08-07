899d5bdf76263a65db407dd60cfcbeb285494fc5
Ian Andreu <ianandreu87@gmail.com>
2015-08-06 20:41:23 -0400

    hit/stand buttons active, game can end

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -135,8 +135,11 @@
 //   console.log("we good")
 //   Dealer()
 //   var dealerIan = new Dealer()
-//   dealerIan.evalulateHand();
+  dealerIan.evalulateHand();
   dealerIan.bindCards();
+  dealerIan.hitMe();
+  dealerIan.checkWinOnHit();
+  dealerIan.checkWinOnStand();
 
 })
 var faceCards = ["jack","queen","king","ace"];
@@ -153,16 +156,16 @@ function Card(symbol, suit, value, image){
 
 function MakeDeck() {
       for (var i = 2; i < 11; i++) {
-        deck.push( new Card( i.toString(), suits[0], i, i+ "_of_" + suits[0] + ".png"   ) );
-        deck.push( new Card( i.toString(), suits[1], i, i+ "_of_" + suits[1] + ".png"   ) );
-        deck.push( new Card( i.toString(), suits[2], i, i+ "_of_" + suits[2] + ".png"   ) );
-        deck.push( new Card( i.toString(), suits[3], i, i+ "_of_" + suits[3] + ".png"   ) );
+        deck.push( new Card( i.toString(), suits[0], i, "cards/" + i+ "_of_" + suits[0] + ".png"   ) );
+        deck.push( new Card( i.toString(), suits[1], i, "cards/" + i+ "_of_" + suits[1] + ".png"   ) );
+        deck.push( new Card( i.toString(), suits[2], i, "cards/" + i+ "_of_" + suits[2] + ".png"   ) );
+        deck.push( new Card( i.toString(), suits[3], i, "cards/" + i+ "_of_" + suits[3] + ".png"   ) );
       }
       for (var j = 0; j < 4; j++) {
-          deck.push( new Card( faceCards[j], suits[0], (faceCards[j] == 'A' ? 11 : 10), faceCards[j] + "_of_" + suits[0] + ".png"       ) );
-          deck.push( new Card( faceCards[j], suits[1], (faceCards[j] == 'A' ? 11 : 10), faceCards[j] + "_of_" + suits[1] + ".png"       ) );
-          deck.push( new Card( faceCards[j], suits[2], (faceCards[j] == 'A' ? 11 : 10), faceCards[j] + "_of_" + suits[2] + ".png"       ) );
-          deck.push( new Card( faceCards[j], suits[3], (faceCards[j] == 'A' ? 11 : 10), faceCards[j] + "_of_" + suits[3] + ".png"       ) );
+          deck.push( new Card( faceCards[j], suits[0], (faceCards[j] == 'ace' ? 11 : 10),"cards/" + faceCards[j] + "_of_" + suits[0] + ".png"       ) );
+          deck.push( new Card( faceCards[j], suits[1], (faceCards[j] == 'ace' ? 11 : 10),"cards/" + faceCards[j] + "_of_" + suits[1] + ".png"       ) );
+          deck.push( new Card( faceCards[j], suits[2], (faceCards[j] == 'ace' ? 11 : 10),"cards/" + faceCards[j] + "_of_" + suits[2] + ".png"       ) );
+          deck.push( new Card( faceCards[j], suits[3], (faceCards[j] == 'ace' ? 11 : 10),"cards/" + faceCards[j] + "_of_" + suits[3] + ".png"       ) );
       }
 
 
@@ -176,6 +179,7 @@ function Dealer(){
     this.deck = new MakeDeck();
     this.dealerHand = [];
     this.playerHand = [];
+    this.hitPool = [];
     this.playerValue=0;
     this.dealerValue=0;
     this.shuffleDeck = function shuffleDeck(){
@@ -200,12 +204,11 @@ var dealerIan = new Dealer();
 dealerIan.deck;
 dealerIan.shuffleDeck();
 Dealer.prototype.dealCards = function(){
-    this.dealerHand.push(deck.splice(0,1)[0]);
+    this.dealerHand.push(dealerIan.deck.splice(0,1)[0]);
     //this.dealerHand.push(deck.splice(0,1)[0]);
-    this.dealerHand.push(deck.splice(0,1)[0]);
-    this.playerHand.push(deck.splice(0,1)[0]);
-    this.playerHand.push(deck.splice(0,1)[0]);
-
+    this.dealerHand.push(dealerIan.deck.splice(0,1)[0]);
+    this.playerHand.push(dealerIan.deck.splice(0,1)[0]);
+    this.playerHand.push(dealerIan.deck.splice(0,1)[0]);
     //console.log(this.dealerHand);
     //console.log(this.playerHand);
     //console.log(deck.length);
@@ -214,48 +217,96 @@ Dealer.prototype.dealCards = function(){
 dealerIan.dealCards();
 
 Dealer.prototype.evalulateHand = function(){
-    //console.log(this.dealerHand[0].value);
+
+    // this.playerValue = this.playerHand.reduce(function(a, b) {
+    //   console.log(this.playerValue)
+    //     return a.value + b.value;
+    //   });
+
+    var tempSumPlayer = 0;
+    var tempSumDealer = 0;
+    console.log(this.dealerHand[0].value);
     console.log(this.dealerHand);
     console.log("");
     console.log(this.playerHand);
     for (var i = 0; i < this.dealerHand.length; i++){
         //console.log(this.dealerHand[i].value)
-        this.dealerValue = this.dealerValue + this.dealerHand[i].value;
+
+        tempSumDealer += this.dealerHand[i].value;
     }
     for (var j = 0; j < this.playerHand.length; j++){
-        this.playerValue = this.playerValue + this.playerHand[j].value;
+        //this.playerValue += this.playerHand[j].value;
+        tempSumPlayer += this.playerHand[j].value;
     }
+    this.dealerValue = tempSumDealer;
+    this.playerValue = tempSumPlayer
 
     console.log(this.dealerValue);
     console.log(this.playerValue);
-  //  console.log(dealerCard1=this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit)
+   console.log(dealerCard1=this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit)
 };
-
-dealerIan.evalulateHand();
+//dealerIan.playerHand[0].value
+//dealerIan.evalulateHand();
 
 Dealer.prototype.hitMe = function(){
+  $('.hit').on('click', function(e){
+      e.preventDefault();
+      var newCard = dealerIan.deck.splice(0,1)[0]
+      dealerIan.playerHand.push(newCard)
+      dealerIan.evalulateHand()
+      //this.hitPool.push(dealerIan.deck.splice(0,1)[0]);
+      $(".player-cards").append($("<img>").addClass("playing-cards").attr('src', newCard.image ))
 
-  console.log(this.dealerHand)
+    });
+  }
 //TODO
+
+Dealer.prototype.checkWinOnHit = function(){
+    $(".hit").on("click", function(e){
+      e.preventDefault();
+      if (dealerIan.playerValue === 21){
+        alert ("21, you win!!!")
+      }
+      else if (dealerIan.playerValue > 21){
+        alert ("Bust")
+      }
+    })
+};
+dealerIan.checkWinOnHit()
+Dealer.prototype.checkWinOnStand = function(){
+  $(".stand").on("click", function(e){
+    e.preventDefault();
+    console.log(dealerIan.playerValue)
+    if (dealerIan.playerValue > dealerIan.dealerValue){
+      alert ("You win!!")
+    }
+    else if (dealerIan.playerValue < dealerIan.dealerValue){
+      alert ("You losseeeeeee")
+    }
+  })
+};
+dealerIan.checkWinOnStand()
   //if player presses hit button, new card is added to hand
   //recalculates value of hand
   //if value > 21. return bust, game over
   // else if value === 21 return win, game over
   // else if value < 21, loop back to line 222
-}
-Dealer.prototype.cardURL = function(){
-  return ("cards/"+this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit+".png")
-}
-dealerIan.cardURL()
+
+// Dealer.prototype.cardURL = function(){
+//   return ("cards/"+this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit+".png")
+// }
+// dealerIan.cardURL()
 
 
 //var test = this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit+".png"
 Dealer.prototype.bindCards = function(){
   //for (){
-    dealerCard1=this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit
+    //dealerCard1=this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit
     ///3_of_spades
-    $(".dealer-cards").append($("<img>").addClass("playing-cards").attr('src', this.cardURL() ))
-    $(".dealer-cards").append($("<img>").addClass("playing-cards").attr('src',"cards/3_of_clubs.png"))
+    $(".dealer-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.dealerHand[0].image ))
+    $(".dealer-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.dealerHand[1].image ))
+    $(".player-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.playerHand[0].image ))
+    $(".player-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.playerHand[1].image ))
   //}
 }
-//dealerIan.bindCards()
+dealerIan.hitMe()
