c426989c1d4f5741121bc97a9b4555379417e5e3
Ian Andreu <ianandreu87@gmail.com>
2015-08-09 16:39:54 -0400

    responsiveness improved Icons added

--- Readme.md
+++ Readme.md
@@ -1,7 +1,19 @@
 ## Blackjack
-A wonderful game for everyone.
+A wonderful game for young and old. Now featuring koalas and margays.
+![](http://i.imgur.com/WH5NYUE.jpg)
+
 ## User Story
 - As a user I want to play a hand of blackjack against a computer opponent so that I can become a better player.
-- As a user I want the interface to be attractive so I don't have to stare at text.
+- As a user I want visual representation of the cards so I don't have to stare at just text.
+- As a user I want to choose to have multiple decks dealt so I can practice counting cards.
 - As a user I want to have a bankroll in the game to track my progress in my playing session.
 - As a user I want to be able to doubledown and split so that the game is more realistic to casino conditions.
+
+## Gameplay
+![](http://g.recordit.co/XnAEMBMt9j.gif)
+
+## Technologies
+- HTML
+- CSS
+- Javascript
+- jQuery
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,135 +1,3 @@
-// function Deck(){
-//     var face = ["J","Q","K","A"];
-//     var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-//     var suits = ["Spades","Diamonds","Hearts","Clubs"];
-//     var deck = [];
-//
-//     for (var i = 0; i <face.length;i++){
-//         //create face cards
-//         deck.push(face[i]+" of " + suits[0]);
-//         deck.push(face[i]+" of " + suits[1]);
-//         deck.push(face[i]+" of " + suits[2]);
-//         deck.push(face[i]+" of " + suits[3]);
-//     }
-//     for (var j = 0; j < numbers.length;j++){
-//         deck.push(numbers[j]+ " of " + suits[0]);
-//         deck.push(numbers[j]+ " of " + suits[1]);
-//         deck.push(numbers[j]+ " of " + suits[2]);
-//         deck.push(numbers[j]+ " of " + suits[3]);
-//     }
-//      return deck;
-// }
-// function Dealer(){
-//     this.deck = new Deck();
-//     this.dealerHand = [];
-//     this.playerHand = [];
-//     this.shuffleDeck = function shuffleDeck(){
-//         var deckLength = this.deck.length;
-//         var t;
-//         var randNumber;
-//         while (deckLength) {
-// // While there remain elements to shuffle…
-//             randNumber = Math.floor(Math.random() * deckLength--);
-// // generates random number with a max value of the deck length, then subtracts 1 from decklength
-//             t = this.deck[deckLength];
-//             // And swap it with the current element.
-//             this.deck[deckLength] = this.deck[randNumber];
-//             this.deck[randNumber] = t;
-//         }
-//         return this.deck;
-//     };
-// }
-// Dealer.prototype.dealCards = function(){
-//     this.dealerHand.push(this.deck.splice(0,1)[0]);
-//     this.dealerHand.push(this.deck.splice(0,1)[0]);
-//     this.playerHand.push(this.deck.splice(0,1)[0]);
-//     this.playerHand.push(this.deck.splice(0,1)[0]);
-// //    console.log(this.dealerHand, this.playerHand);
-//
-// };
-//
-// var dealerJimmy = new Dealer();
-// newDeck = dealerJimmy.shuffleDeck();
-// newDeck;
-// dealerJimmy.dealCards();
-// dealerJimmy.dealerHand;
-//
-//
-//
-// Dealer.prototype.handEvaluator = function(){
-//     return this.dealerHand[0]
-//
-// }
-// dealerJimmy.handEvaluator()
-//
-// ////////////////////////////////////////////
-// var faceCards = ["J","Q","K","A"];
-// var suits = ["Spades","Diamonds","Hearts","Clubs"];
-// var deck = [];
-//
-// function Card(symbol, suit, value){
-//     this.symbol = symbol;
-//     this.suit = suit;
-//     this.value = value;
-//     }
-//
-// function MakeDeck() {
-//       for (var i = 2; i < 11; i++) {
-//         deck.push( new Card( i.toString(), suits[0], i) );
-//         deck.push( new Card( i.toString(), suits[1], i) );
-//         deck.push( new Card( i.toString(), suits[2], i) );
-//         deck.push( new Card( i.toString(), suits[3], i) );
-//       }
-//       for (var j = 0; j < 4; j++) {
-//         for (var k = 0; k < 4; k++) {
-//           deck.push( new Card( faceCards[j], suits[k], (faceCards[j] == 'A' ? 11 : 10)) );
-//         }
-//       }return deck;
-//     }
-//
-// //makeDeck();
-//
-// function Dealer(){
-//     this.deck = new MakeDeck();
-//     this.dealerHand = [];
-//     this.playerHand = [];
-//     this.shuffleDeck = function shuffleDeck(){
-//         var deckLength = this.deck.length;
-//         var t;
-//         var randNumber;
-//         while (deckLength) {
-// // While there remain elements to shuffle…
-//             randNumber = Math.floor(Math.random() * deckLength--);
-// // generates random number with a max value of the deck length, then subtracts 1 from decklength
-//             t = this.deck[deckLength];
-//             // And swap it with the current element.
-//             this.deck[deckLength] = this.deck[randNumber];
-//             this.deck[randNumber] = t;
-//         }
-//         return this.deck;
-//     };
-// }
-//
-// var dealerIan = new Dealer();
-//
-// dealerIan.deck[0];
-// dealerIan.shuffleDeck();
-// Dealer.prototype.dealCards = function(){
-//     this.dealerHand.push(deck.splice(0,1)[0]);
-//     this.dealerHand.push(deck.splice(0,1)[0]);
-//     this.playerHand.push(deck.splice(0,1)[0]);
-//     this.playerHand.push(deck.splice(0,1)[0]);
-//
-//     console.log(this.dealerHand);
-//     console.log(this.playerHand);
-//     console.log(deck.length);
-// };
-//
-// dealerIan.dealCards();
-
-
-
-
 $(document).ready(function(){
   $(".new-deal").on('click', function(){
     dealerIan.newGame();
@@ -137,13 +5,14 @@ $(document).ready(function(){
   var faceCards = ["jack","queen","king","ace"];
   var suits = ["spades","diamonds","hearts","clubs"];
   var deck = [];
-
+  //cards will be objects with value and image-link attributes to make referencing easier.
   function Card(symbol, suit, value, image){
     this.symbol = symbol;
     this.suit = suit;
     this.value = value;
     this.image = image;
   }
+
   function MakeDeck() {
     for (var i = 2; i < 11; i++) {
       deck.push( new Card( i.toString(), suits[0], i, "cards/" + i+ "_of_" + suits[0] + ".png"   ) );
@@ -160,8 +29,6 @@ $(document).ready(function(){
     return deck;
   }
 
-  //makeDeck();
-
   function Dealer(){
       this.deck = new MakeDeck();
       this.dealerHand = [];
@@ -173,9 +40,9 @@ $(document).ready(function(){
           var t;
           var randNumber;
           while (deckLength) {
-  // While there remain elements to shuffle…
+            // While there remain elements to shuffle…
               randNumber = Math.floor(Math.random() * deckLength--);
-  // generates random number with a max value of the deck length, then subtracts 1 from decklength
+              // generates random number with a max value of the deck length, then subtracts 1 from decklength
               t = this.deck[deckLength];
               // And swap it with the current element.
               this.deck[deckLength] = this.deck[randNumber];
@@ -184,91 +51,68 @@ $(document).ready(function(){
           return this.deck;
       };
   }
-
   var dealerIan = new Dealer();
 
-  //dealerIan.deck;
-  //dealerIan.shuffleDeck();
   Dealer.prototype.dealCards = function(){
+      //will deal initial 3 cards for game start.
       dealerIan.shuffleDeck();
       this.dealerHand.push(dealerIan.deck.splice(0,1)[0]);
-      //this.dealerHand.push(deck.splice(0,1)[0]);
-    //  this.dealerHand.push(dealerIan.deck.splice(0,1)[0]);
       this.playerHand.push(dealerIan.deck.splice(0,1)[0]);
       this.playerHand.push(dealerIan.deck.splice(0,1)[0]);
-      //console.log(this.dealerHand);
-      //console.log(this.playerHand);
-      //console.log(deck.length);
   };
 
-  // dealerIan.dealCards();
-
   Dealer.prototype.evaluateHand = function(){
+    //loops through dealer/player hands and sums values
     var tempSumPlayer = 0;
     var tempSumDealer = 0;
-    console.log(this.dealerHand[0].value);
-    console.log(this.dealerHand);
-    console.log("");
-    console.log(this.playerHand);
+    // console.log(this.dealerHand);
+    // console.log("^^^dealers hand^^^");
+    // console.log(this.playerHand);
     for (var i = 0; i < this.dealerHand.length; i++){
-        //console.log(this.dealerHand[i].value)
-
       tempSumDealer += this.dealerHand[i].value;
     }
     for (var j = 0; j < this.playerHand.length; j++){
-        //this.playerValue += this.playerHand[j].value;
       tempSumPlayer += this.playerHand[j].value;
     }
     this.dealerValue = tempSumDealer;
     this.playerValue = tempSumPlayer;
-
-    console.log(this.dealerValue);
-    console.log(this.playerValue);
-    console.log(dealerCard1=this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit);
+    // console.log("dealers value: " + this.dealerValue);
+    // console.log("players value: " + this.playerValue);
   };
-  //dealerIan.playerHand[0].value
-  //dealerIan.evaluateHand();
 
   Dealer.prototype.hitMe = function(){
+    //deals one card to player. pushes to hand, appends image to screen.
     $('.hit').on('click', function(){
       var newCard = dealerIan.deck.splice(0,1)[0];
       dealerIan.playerHand.push(newCard);
       dealerIan.evaluateHand();
-      //this.hitPool.push(dealerIan.deck.splice(0,1)[0]);
       $(".player-cards").append($("<img>").addClass("playing-cards").attr('src', newCard.image ));
       dealerIan.displayValues();
     });
   };
+
   Dealer.prototype.checkWin = function(){
+    //checks for winner and displays relavant message div
     dealerIan.evaluateHand();
     if (dealerIan.dealerValue === dealerIan.playerValue){
       $(".messages").text("PUSH").show();
       setTimeout(function() { $(".messages").hide(); }, 1500);
     }
     else if (dealerIan.dealerValue === 21){
-      //alert("You lose, dealer 21");
       $(".messages").text("YOU LOSE").show();
       setTimeout(function() { $(".messages").hide(); }, 1500);
     }
     else if (dealerIan.playerValue === 21){
-      //alert("You win, 21!");
       $(".top-messages").text("PLAYER 21").show();
       setTimeout(function() { $(".top-messages").hide(); }, 1500);
       $(".messages").text("YOU WIN").show();
       setTimeout(function() { $(".messages").hide(); }, 1500);
     }
     else if (dealerIan.playerValue > 21){
-      // for (var i = 0; i < dealerIan.playerHand.length; i++){
-      //   if(dealerIan.playerHand[i].value === 11 ){
-      //     dealerIan.playerHand[i].value
-      //
-      //   }
-      // }
       $(".messages").text("BUSTED").show();
       setTimeout(function() { $(".messages").hide(); }, 1500);
     }
     else if(dealerIan.dealerValue > 21){
-      //alert("Dealer busts, you win!");
       $(".top-messages").text("DEALER BUST").show();
       setTimeout(function() { $(".top-messages").hide(); }, 1500);
       $(".messages").text("YOU WIN").show();
@@ -283,27 +127,6 @@ $(document).ready(function(){
       setTimeout(function() { $(".messages").hide(); }, 1500);
       }
     };
-
-  // Dealer.prototype.ace = function(value){
-  //   if (value > 21){
-  //     for (var i = 0; i < dealerIan.playerHand.length; i++){
-  //       if(dealerIan.playerHand[i].value === 11 ){
-  //         dealerIan.playerValue = dealerIan.playerValue - 10
-  //         //dealerIan.evaluateHand()
-  //         return value;
-  //       }
-  //     }
-  //   }
-  // }
-  // Dealer.prototype.checkAcePlayer = function(){
-  //   for (var i = 0; i < dealerIan.playerHand.length; i++){
-  //     if(dealerIan.playerHand[i].value === 11 ){
-  //       return true
-  //     }
-  //     else{
-  //       return false
-  //       }
-  // }
 
   Dealer.prototype.checkWinOnHit = function(){
     $(".hit").on("click", function(){
@@ -321,13 +144,9 @@ $(document).ready(function(){
   };
   Dealer.prototype.checkWinOnStand = function(){
     $(".stand").on("click", function(){
-    //  debugger;
       $(".back-card").remove();
       var newCard = dealerIan.deck.splice(0,1)[0];
       dealerIan.evaluateHand();
-      // if (dealerIan.dealerValue === dealerIan.playerValue){
-      //   alert("Tie")
-      // }
       if (dealerIan.dealerValue > 16){
         return dealerIan.checkWin();
       }
@@ -342,23 +161,21 @@ $(document).ready(function(){
       $(".dealer-value").html(dealerIan.dealerValue);
     });
   };
-  //var test = this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit+".png"
+
   Dealer.prototype.bindCards = function(){
-    //for (){
-      //dealerCard1=this.dealerHand[0].symbol +"_of_"+ this.dealerHand[0].suit
-      ///3_of_spades
+    //imagebinding for initial 3 card deal at gamestart
     $(".dealer-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.dealerHand[0].image ));
-    //  $(".dealer-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.dealerHand[1].image ));
     $(".player-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.playerHand[0].image ));
     $(".player-cards").append($("<img>").addClass("playing-cards").attr('src', dealerIan.playerHand[1].image ));
-    //}
   };
 
   Dealer.prototype.displayValues = function(){
+    //display player hand value
     $(".player-value").html(dealerIan.playerValue);
-    //$(".dealer-value").html(dealerIan.dealerValue)
   };
+
   Dealer.prototype.icons = function(){
+    //player & dealer icon display on mouse input
     $(".dealer-name-plate").mouseenter(function(){
       $(".koala").show();
     });
@@ -372,23 +189,24 @@ $(document).ready(function(){
       $(".margay").hide();
     });
   };
-  //dealerIan.displayValues()
-  //dealerIan.hitMe()
-
+  //call functions that only need to be called once and hide name plates before new game is started
   dealerIan.hitMe();
   dealerIan.checkWinOnHit();
   dealerIan.checkWinOnStand();
   $(".dealer-name-plate").hide();
   $(".player-name-plate").hide();
+
   Dealer.prototype.newGame = function(){
-    //debugger
+    $(".blackjack").hide();
     $(".dealer-name-plate").show();
     $(".player-name-plate").show();
+    //emptys previous hands on new deal
     dealerIan.playerHand = [];
     dealerIan.dealerHand = [];
     $(".dealer-cards").empty();
     $(".player-cards").empty();
     $(".dealer-value").empty();
+    //back card image for hidden dealer card
     $(".dealer-cards").append($('<img src="cards/backcard.jpg">').addClass("back-card"));
     dealerIan.shuffleDeck();
     dealerIan.dealCards();
